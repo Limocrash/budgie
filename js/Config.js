55c599c1e5560d98337e1c7e64c61a5976e9d690
Limocrash <54497041+Limocrash@users.noreply.github.com>
2025-04-18 21:52:16 +0800

    Intitial Config.js added

---
+++ js/Config.js
@@ -0,0 +1,8 @@
+// Budgie Config - Auto-generated from Config! sheet
+const BUDGIE_CONFIG = {
+    "FORM_EXPENSE_ID": "https://docs.google.com/forms/d/1FAIpQLSd_N4OaQlYBKPUtZjHcVmd-6hduhPM8guJ7OPg7eKAptyUPXg/viewform",
+    "FORM_LOAN_EXISTING_ID": "https://docs.google.com/forms/d/1FAIpQLSdcyakPKi4-e3d7tlsnwwyQWaiT0zyc0AO8J8WvCrS2iT4E8g/viewform",
+    "FORM_LOAN_NEW_ID": "https://docs.google.com/forms/d/1FAIpQLSdMA8m2nyOgGe4IEwqSuBLTdW_I3rOmpzWObeg--zRkPM7FYw/viewform",
+    "FORM_REPAYMENT_ID": "https://docs.google.com/forms/d/1FAIpQLSep5ObSuedyc9W9P1aHVcZq3bd92x8Y0QL2BqtBveBktM0IrA/viewform",
+    "BUDGIE_SITE_URL": "https://limocrash.github.io/budgie"
+  }
