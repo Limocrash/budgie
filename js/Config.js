b33f0501860c9f645070587b98396f1e29feb1ef
Limocrash <54497041+Limocrash@users.noreply.github.com>
2025-04-20 07:18:39 +0800

    Changed CONFIG.js and index.html to add VIEW_EXPENSES

--- js/Config.js
+++ js/Config.js
@@ -1,9 +1,10 @@
-// Budgie Config - Auto-generated from Config! sheet
+// Budgie Config - Auto-generated from Config! sheet 
 const BUDGIE_CONFIG = {
   "FORM_EXPENSE_ID": "https://docs.google.com/forms/d/e/1FAIpQLSd_N4OaQlYBKPUtZjHcVmd-6hduhPM8guJ7OPg7eKAptyUPXg/viewform",
   "FORM_LOAN_EXISTING_ID": "https://docs.google.com/forms/d/e/1FAIpQLSdcyakPKi4-e3d7tlsnwwyQWaiT0zyc0AO8J8WvCrS2iT4E8g/viewform",
   "FORM_LOAN_NEW_ID": "https://docs.google.com/forms/d/e/1FAIpQLSdMA8m2nyOgGe4IEwqSuBLTdW_I3rOmpzWObeg--zRkPM7FYw/viewform",
   "FORM_REPAYMENT_ID": "https://docs.google.com/forms/d/e/1FAIpQLSep5ObSuedyc9W9P1aHVcZq3bd92x8Y0QL2BqtBveBktM0IrA/viewform",
   "BUDGIE_SITE_URL": "https://limocrash.github.io/budgie",
-  "SCRIPT_WEBAPP_URL": "https://docs.google.com/spreadsheets/d/1AStIoowJuZX2enGOCrvLwnG4F4Ypg9VK5NZp-oDE8yo/edit?usp=sharing"
+  "SCRIPT_WEBAPP_URL": "https://docs.google.com/spreadsheets/d/1AStIoowJuZX2enGOCrvLwnG4F4Ypg9VK5NZp-oDE8yo/edit?usp=sharing",
+  "VIEW_EXPENSES": "https://docs.google.com/spreadsheets/d/e/2PACX-1vRK73zqWFd7Wr4owrfwOocMWBQfRD5uCulUasdqGtVAYUnLXGcL71vgMXFKPCbXs_UWuj8pG4Izh0bm/pubhtml?gid=439769693&single=true"
 };
