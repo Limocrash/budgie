f2ed026350ba6db4154879d57dd73f90235f0999
Limocrash <54497041+Limocrash@users.noreply.github.com>
2025-04-21 12:11:38 +0800

    Changed Config.js to Version 0.1.2 Added VIew_Expenses_CSV URL

--- js/Config.js
+++ js/Config.js
@@ -1,4 +1,5 @@
-// Budgie Config - Auto-generated from Config! sheet 
+// Budgie Config - Auto-generated from Config! sheet
+// Version 0.1.2 Added VIew_Expenses_CSV URL
 const BUDGIE_CONFIG = {
   "FORM_EXPENSE_ID": "https://docs.google.com/forms/d/e/1FAIpQLSd_N4OaQlYBKPUtZjHcVmd-6hduhPM8guJ7OPg7eKAptyUPXg/viewform",
   "FORM_LOAN_EXISTING_ID": "https://docs.google.com/forms/d/e/1FAIpQLSdcyakPKi4-e3d7tlsnwwyQWaiT0zyc0AO8J8WvCrS2iT4E8g/viewform",
@@ -6,5 +7,6 @@ const BUDGIE_CONFIG = {
   "FORM_REPAYMENT_ID": "https://docs.google.com/forms/d/e/1FAIpQLSep5ObSuedyc9W9P1aHVcZq3bd92x8Y0QL2BqtBveBktM0IrA/viewform",
   "BUDGIE_SITE_URL": "https://limocrash.github.io/budgie",
   "SCRIPT_WEBAPP_URL": "https://docs.google.com/spreadsheets/d/1AStIoowJuZX2enGOCrvLwnG4F4Ypg9VK5NZp-oDE8yo/edit?usp=sharing",
-  "VIEW_EXPENSES": "https://docs.google.com/spreadsheets/d/e/2PACX-1vRK73zqWFd7Wr4owrfwOocMWBQfRD5uCulUasdqGtVAYUnLXGcL71vgMXFKPCbXs_UWuj8pG4Izh0bm/pubhtml?gid=439769693&single=true"
+  "VIEW_EXPENSES": "https://docs.google.com/spreadsheets/d/e/2PACX-1vRK73zqWFd7Wr4owrfwOocMWBQfRD5uCulUasdqGtVAYUnLXGcL71vgMXFKPCbXs_UWuj8pG4Izh0bm/pubhtml?gid=439769693&single=true",
+  "VIEW_EXPENSES_CSV": "https://docs.google.com/spreadsheets/d/1AStIoowJuZX2enGOCrvLwnG4F4Ypg9VK5NZp-oDE8yo/gviz/tq?tqx=out:csv&sheet=ExpensesView"
 };
