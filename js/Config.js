f01aa1c1033b230240b10cf60523224f2b1b2032
Limocrash <54497041+Limocrash@users.noreply.github.com>
2025-04-22 20:57:59 +0800

    V0.1.10.4 Fix: Something about fetch and gviz

--- js/Config.js
+++ js/Config.js
@@ -1,5 +1,5 @@
 // Budgie Config - Auto-generated from Config! sheet
-const BUDGIE_CONFIG = {
+window.BUDGIE_CONFIG = {
   "FORM_EXPENSE_ID": "https://docs.google.com/forms/d/e/1FAIpQLSd_N4OaQlYBKPUtZjHcVmd-6hduhPM8guJ7OPg7eKAptyUPXg/viewform",
   "FORM_LOAN_EXISTING_ID": "https://docs.google.com/forms/d/e/1FAIpQLSdcyakPKi4-e3d7tlsnwwyQWaiT0zyc0AO8J8WvCrS2iT4E8g/viewform",
   "FORM_LOAN_NEW_ID": "https://docs.google.com/forms/d/e/1FAIpQLSdMA8m2nyOgGe4IEwqSuBLTdW_I3rOmpzWObeg--zRkPM7FYw/viewform",
@@ -10,3 +10,4 @@ const BUDGIE_CONFIG = {
   "VIEW_EXPENSES_CSV": "https://docs.google.com/spreadsheets/d/1AStIoowJuZX2enGOCrvLwnG4F4Ypg9VK5NZp-oDE8yo/gviz/tq?tqx=out:csv&sheet=Form%20Responses%206",
   "VIEW_EXPENSES_PAGE": "https://limocrash.github.io/budgie/viewExpenses.html"
 };
+console.log("Budgie Config loaded:", window.BUDGIE_CONFIG);
